c1a39a0cbb23cd574e3e4ae193adccca33c60f1e
Vicario Williams <vawilliams@github.com>
2021-10-04 04:52:09 +0200

    JTA: Added app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent    } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GameComponent   } from './components/game/game.component';
+import { ButtonComponent } from './components/button/button.component';
+import { ScoreComponent  } from './components/score/score.component';
+import { BoardComponent  } from './components/board/board.component';
+
+const routes: Routes = [
+  { path: '', component: GameComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    GameComponent,
+    ScoreComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
