69eecd3079daf38ed1206480014ef9b614b4825e
Vicario Williams <vawilliams@github.com>
2021-10-05 23:58:05 +0200

    JTA: Updated view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui/ui.service';
 
 @Component({
   selector: 'app-header',
@@ -11,17 +13,26 @@ export class HeaderComponent implements OnInit {
     warning: '#F4A236',
     error: '#D4190C'
   }
-  constructor() { }
+  isGameInProgress!: boolean;
+  isGameDone!: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .updateUi()
+      .subscribe(isGameInProgress => this.isGameInProgress = isGameInProgress);
+  }
 
   ngOnInit(): void {
+    this.isGameDone = false;
   }
 
   restartGame() {
-
+    location.reload();
   }
 
   exitGame() {
-    
+    window.close();
   }
 
 }
